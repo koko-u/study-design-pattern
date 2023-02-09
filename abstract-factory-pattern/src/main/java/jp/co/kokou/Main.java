39712e8174b6d488ac60ad22f4f17459d05d91fd
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2023-02-10 07:25:31 +0900

    Abstract Factory パターンのプロジェクトを作成する

---
+++ abstract-factory-pattern/src/main/java/jp/co/kokou/Main.java
@@ -0,0 +1,10 @@
+package jp.co.kokou;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        log.info("Abstract Factory Pattern");
+    }
+}
